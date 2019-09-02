7e1733c3c16899f54ffa5ad5aafae30610bcffbf
Sam Green <sam.green81@gmail.com>
2019-09-02 10:24:00 +0100

    Minor improvements

--- esrgan.py
+++ esrgan.py
@@ -1,3 +1,4 @@
+import re
 import glob
 import cv2
 import numpy as np
@@ -6,6 +7,7 @@ import architecture as arch
 import argparse
 import warnings
 from pathlib import Path
+from sys import exit
 
 
 class SmartFormatter(argparse.HelpFormatter):
@@ -100,7 +102,8 @@ def main():
         "jpeg100": "1x_JPEG_80-100.pth",
         "box": "4x_Box.pth",
         "misc": "4x_Misc_220000.pth",
-        "face": "4x_face_focus_275k.pth",
+        "facefocus": "4x_face_focus_275k.pth",
+        "face": "4x_Faces_N_250000.pth",
         "fatality": "4x_Fatality_01_265000_G.pth",
         "unholy": "4x_unholy03.pth",
         "waifugan": "4x_WaifuGAN_v3_30000.pth",
@@ -112,6 +115,8 @@ def main():
         "detoon_alt": "4x_detoon_alt.pth",
         "bc1r": "1x_BC1_take2_260850.pth",
         "bc1f": "1x_BC1NoiseAgressiveTake3_400000_G.pth",
+        "aa": "1x_Alias_200000_G.pth",
+        "dedither": "1x_DEDITHER_32_512_126900_G.pth",
     }
     model_docs = {
         "RRDB_ESRGAN_x4.pth": "Official perceptual upscaling model.",
@@ -120,6 +125,7 @@ def main():
         "4x_interp_09.pth": "RRDB_PSNR_x4 interpolated with RRDB_ESRGAN_x4 with 0.9 strength.",
         "4x_Box.pth": "General purpose upscaling. Larger dataset than RRDB_ESRGAN_x4.",
         "4x_Misc_220000.pth": "Surface upscaling. Works well as general/manga upscaler too.",
+        "4x_Faces_N_250000.pth": "Face upscaling.",
         "4x_face_focus_275k.pth": "Face deblurring and upscaling.",
         "4x_Fatality_01_265000_G.pth": "Upscales pixel art.",
         "4x_rebout_325k.pth": "Upscales pixel art. Trained on KOF94 Rebout.",
@@ -128,6 +134,9 @@ def main():
         "4x_unholy03.pth": "Manga upscaling. Interpolation of many models.",
         "4x_WaifuGAN_v3_30000.pth": "Manga upscaling.",
         "4x_Manga109Attempt.pth": "Manga upscaling.",
+        "4x_ESRGAN_Skyrim_NonTiled_Alpha_NN_128_32_105000.pth": "Upscales greyscale maps. Trained on Skyrim textures.",
+        "4x_detoon_225k.pth": "Tries to make toon images realistic.",
+        "4x_detoon_alt.pth": "Tries to make toon images realistic. Softer version.",
         "1x_JPEG_00-20.pth": "Cleans up JPEG compression. For images with 0-20%% compression ratio.",
         "1x_JPEG_20-40.pth": "Cleans up JPEG compression. For images with 20-40%% compression ratio.",
         "1x_JPEG_40-60.pth": "Cleans up JPEG compression. For images with 40-60%% compression ratio.",
@@ -135,29 +144,37 @@ def main():
         "1x_JPEG_80-100.pth": "Cleans up JPEG compression. For images with 80-100%% compression ratio.",
         "1x_BC1_take2_260850.pth": "Cleans up BC1 compression. Restricted version (only works with low-noise images).",
         "1x_BC1NoiseAgressiveTake3_400000_G.pth": "Cleans up BC1 compression. Free version (more aggressive than restricted).",
+        "1x_cinepak_200000.pth": "Cleans up Cinepak, msvideo1 and Roq compression.",
         "1x_DeSharpen.pth": "Removes over-sharpening.",
         "1x_normals_generator_general_215k.pth": "Attempts to generate a normal map from a texture.",
+        "1x_Alias_200000_G.pth": "Performs anti-aliasing on the image.",
+        "1x_DEDITHER_32_512_126900_G.pth": "Tries to remove dithering patterns.",
     }
     models = enum_models(model_dir, aliases)
     models_help = get_models_help(models, aliases, model_docs)
     args = parse_args(models, models_help)
-    device = torch.device(args.device)
     model_path = model_dir / models[args.model]
 
+    scale_pattern = re.compile('(\d+)x')
+    scale_match = scale_pattern.match(model_path.stem)
+    scale = int(scale_match.group(1)) if scale_match else 4
+
+    device = torch.device(args.device)
     model = arch.RRDB_Net(
         3,
         3,
         64,
         23,
         gc=32,
-        upscale=4,
+        upscale=scale,
         norm_type=None,
         act_type="leakyrelu",
         mode="CNA",
         res_scale=1,
         upsample_mode="upconv",
     )
-    model.load_state_dict(torch.load(str(model_path)), strict=True)
+    state_dict = torch.load(model_path)
+    model.load_state_dict(state_dict, strict=True)
     model.eval()
 
     for k, v in model.named_parameters():
@@ -193,7 +210,8 @@ def main():
         cv2.imwrite(str(out_path), img)
 
     print("Done")
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    exit(main())
